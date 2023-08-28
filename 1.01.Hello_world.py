01050d49af8d21a137b050dc19c84ff9902fc9b6
RifatMuhtasim <k.rifat.muhtasim@gmail.com>
2023-08-29 04:37:01 +0600

    Basic Python

--- 1.01.Hello_world.py
+++ 1.01.Hello_world.py
@@ -17,3 +17,28 @@ print(df['Name'])  # Access the 'Name' column
 if 5 > 2:
         print("Number 5 is Greater then the 2")
 
+
+# Install Numpy
+import numpy as np
+
+def main():
+    # Create a NumPy array
+    data = np.array([1, 2, 3, 4, 5])
+
+    # Perform basic operations
+    mean = np.mean(data)
+    median = np.median(data)
+    variance = np.var(data)
+    std_dev = np.std(data)
+
+    # Print the results
+    print("Data:", data)
+    print("Mean:", mean)
+    print("Median:", median)
+    print("Variance:", variance)
+    print("Standard Deviation:", std_dev)
+
+if __name__ == "__main__":
+    main()
+
+
