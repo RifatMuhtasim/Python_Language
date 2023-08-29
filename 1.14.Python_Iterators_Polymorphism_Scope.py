ddbfbed6be561a9653f3316e8744f5f603db9d2b
Rifat Muhtasim <67732890+RifatMuhtasim@users.noreply.github.com>
2023-08-29 06:02:29 +0000

    Basic

--- 1.14.Python_Iterators_Polymorphism_Scope.py
+++ 1.14.Python_Iterators_Polymorphism_Scope.py
@@ -4,3 +4,24 @@ An iterator is an object that can be iterated upon, meaning that you can travers
 Technically, in Python, an iterator is an object which implements the iterator protocol, which consist of the methods __iter__() and __next__().
 '''
 
+print("Hello World")
+
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+# Python Polymorphisom
+mytuple = ("apple", "banana", "cherry")
+print(len(mytuple))
+
+
+# Global Keyword
+def my_fuction():
+    global x
+    x = 10
+my_fuction()
+print(x)
